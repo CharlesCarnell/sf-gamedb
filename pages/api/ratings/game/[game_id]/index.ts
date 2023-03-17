129158398c34718df87c32e241115b02158f0a94
Charles Carnell <me@charlescarnell.co.uk>
2023-03-17 16:05:28 +0000

    Format ratings and multiple instead of a singular

--- pages/api/ratings/game/[game_id]/index.ts
+++ pages/api/ratings/game/[game_id]/index.ts
@@ -2,14 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { RatingRepository } from "@server/repositories";
 import {
   Game,
-  Rating,
+  // Rating,
   User
 } from "@server/models";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   if ( req.query.game_id ) {
-    const rating = await RatingRepository.findOne({
+    const rating = await RatingRepository.findAll({
       where: { game_id: req.query.game_id },
       include: [Game, User]
     });
@@ -17,7 +17,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if ( rating === null ) {
       return res.status(400).json({ error: 'No records found with provided Game ID' })
     }
-    return res.status(200).json({ ...rating.toJSON() });
+
+    const formattedRating = [];
+    for (const [key, value] of Object.entries(rating)) {
+      formattedRating.push( { ...rating[key].dataValues });
+    }
+
+    return res.status(200).json(formattedRating);
+    // return res.status(200).json({ ...rating.toJSON() });
   } else {
     return res.status(400).json({ error: 'Game ID must be supplied' })
   }
