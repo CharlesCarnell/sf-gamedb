90b6ee3bfdacae2f4db944b1352779fcb00dad73
Charles Carnell <me@charlescarnell.co.uk>
2023-03-24 17:22:23 +0000

    Add User ID to JWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,4 +4,13 @@ import NextAuth from 'next-auth';
 
 import { authOptions } from '../../../server/models';
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  ...authOptions,
+  callbacks: {
+    async session({ session, token, user }) {
+      // Add the User ID to their JWT
+      session.user.id = user.id;
+      return session;
+    }
+  }
+});
